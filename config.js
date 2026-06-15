9fe8d54fdd29f6a7645377156965f1fa8cc3d648
awierzbicka125 <63124186+awierzbicka125@users.noreply.github.com>
2026-06-15 18:47:28 +0200

    Configure Supabase frontend connection

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-window.DIET_APP_CONFIG = {
-  supabaseUrl: '',
-  supabaseAnonKey: '',
+﻿window.DIET_APP_CONFIG = {
+  supabaseUrl: 'https://cjdmogmvccfygqelmzwa.supabase.co',
+  supabaseAnonKey: 'sb_publishable_mGoFCNpQR01bFEQXaQecpQ_M7G4jDk3',
   aiEndpoint: ''
 };
